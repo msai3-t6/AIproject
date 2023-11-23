2ce11a0ea99c142224e6f1fa4a935978f6d7151c
ractactia <ractactia@gmail.com>
2023-11-23 16:05:03 +0900

    hello-detection

---
+++ hello-detection_sample/prediction.py
@@ -0,0 +1,37 @@
+######## IMPORTS ##########
+import sounddevice as sd
+from scipy.io.wavfile import write
+import librosa
+import numpy as np
+from tensorflow.keras.models import load_model
+
+####### ALL CONSTANTS #####
+fs = 44100
+seconds = 2
+filename = "prediction.wav"
+class_names = ["Wake Word NOT Detected", "Wake Word Detected"]
+
+##### LOADING OUR SAVED MODEL and PREDICTING ###
+model = load_model("saved_model/WWD_ihyun.h5")
+
+print("Prediction Started: ")
+i = 0
+while True:
+    print("Say Now: ")
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    sd.wait()
+    write(filename, fs, myrecording)
+
+    audio, sample_rate = librosa.load(filename)
+    mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+    mfcc_processed = np.mean(mfcc.T, axis=0)
+
+    prediction = model.predict(np.expand_dims(mfcc_processed, axis=0))
+    if prediction[:, 1] > 0.80:
+        print(f"'안녕' 단어가 검출되었습니다({i})")
+        print("Confidence:", prediction[:, 1])
+        i += 1
+    
+    else:
+        print(f"검출되지 않았습니다")
+        print("Confidence:", prediction[:, 0])
